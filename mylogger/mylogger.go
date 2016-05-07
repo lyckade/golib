e93ca4ffc089a4967bd1a98acfbf51b9c398c909
lyckade <as@lycka.de>
2016-05-07 21:48:48 +0200

    mylogger added

---
+++ mylogger/mylogger.go
@@ -0,0 +1,22 @@
+/*
+Package mylogger is a simple abstraction for the logger
+*/
+package mylogger
+
+import (
+	"log"
+	"os"
+)
+
+//NewFileLogger returns a simple Logger which creates a
+//Logger which writes into a log file
+func NewFileLogger(fileName string, prefix string) *log.Logger {
+	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		f, _ = os.Create(fileName)
+		//panic(fmt.Sprintf("NewFileLogger can not be created, because file could not be opened: %s", err))
+	}
+	//defer f.Close()
+	logger := log.New(f, prefix, log.Lshortfile|log.Ldate|log.Ltime)
+	return logger
+}
