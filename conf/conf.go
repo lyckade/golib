e0824b5a2f3f7e0a43558b58aafd99fc3f356abe
lyckade <as@lycka.de>
2016-05-14 07:03:31 +0200

    Logging over LoadConf now possible
    Added a logger to the constructor

--- conf/conf.go
+++ conf/conf.go
@@ -3,23 +3,24 @@ package conf
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 )
 
 //LoadConf loads the configuration from a json file into a
 //struct
-func LoadConf(confFile string, confStruc interface{}) {
+func LoadConf(confFile string, confStruc interface{}, logger *log.Logger) {
 	var err error
 	var b []byte
 
 	b, err = ioutil.ReadFile(confFile)
-	errorLog(err)
+	errorLog(err, logger)
 	err = json.Unmarshal(b, &confStruc)
-	errorLog(err)
+	errorLog(err, logger)
 
 }
 
-func errorLog(err error) {
+func errorLog(err error, logger *log.Logger) {
 	if err != nil {
-		panic(err)
+		logger.Println(err)
 	}
 }
